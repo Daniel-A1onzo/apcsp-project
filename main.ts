e0cc513c7b2f66f683ab67bbbb0b363d82b9ab25
Daniel-A1onzo <156249399+Daniel-A1onzo@users.noreply.github.com>
2024-02-21 14:54:55 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,139 @@
+namespace SpriteKind {
+    export const HitBox = SpriteKind.create()
+    export const QuestNPC = SpriteKind.create()
+}
+namespace StatusBarKind {
+    export const SkelHP = StatusBarKind.create()
+}
+function MonsterHunt (PlayerLevel: number) {
+    for (let index = 0; index < 10; index++) {
+        Enemies = sprites.create(EnemySprites._pickRandom(), SpriteKind.Enemy)
+        tiles.placeOnTile(Enemies, SpawnLocation.removeAt(randint(0, SpawnLocation.length - 1)))
+    }
+}
+controller.combos.attachCombo("BB", function () {
+    game.showLongText("Level: " + PlayerLevel, DialogLayout.Bottom)
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    MonsterHunt(PlayerLevel)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.QuestNPC, function (sprite, otherSprite) {
+	
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    PlayerLevel += 1
+})
+let Enemies: Sprite = null
+let PlayerLevel = 0
+let SpawnLocation: tiles.Location[] = []
+let EnemySprites: Image[] = []
+EnemySprites = [img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ......fd11111111df......
+    ......fddd1111dddf......
+    ......fbdbfddfbdbf......
+    ......fcdcf11fcdcf......
+    .......fb111111bf.......
+    ......fffcdb1bdffff.....
+    ....fc111cbfbfc111cf....
+    ....f1b1b1ffff1b1b1f....
+    ....fbfbffffffbfbfbf....
+    .........ffffff.........
+    ...........fff..........
+    ........................
+    ........................
+    ........................
+    ........................
+    `, img`
+    . . f f f . . . . . . . . f f f 
+    . f f c c . . . . . . f c b b c 
+    f f c c . . . . . . f c b b c . 
+    f c f c . . . . . . f b c c c . 
+    f f f c c . c c . f c b b c c . 
+    f f c 3 c c 3 c c f b c b b c . 
+    f f b 3 b c 3 b c f b c c b c . 
+    . c b b b b b b c b b c c c . . 
+    . c 1 b b b 1 b b c c c c . . . 
+    c b b b b b b b b b c c . . . . 
+    c b c b b b c b b b b f . . . . 
+    f b 1 f f f 1 b b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    . f b b b b b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `, img`
+    . . . . c c c c c c . . . . . . 
+    . . . c 6 7 7 7 7 6 c . . . . . 
+    . . c 7 7 7 7 7 7 7 7 c . . . . 
+    . c 6 7 7 7 7 7 7 7 7 6 c . . . 
+    . c 7 c 6 6 6 6 c 7 7 7 c . . . 
+    . f 7 6 f 6 6 f 6 7 7 7 f . . . 
+    . f 7 7 7 7 7 7 7 7 7 7 f . . . 
+    . . f 7 7 7 7 6 c 7 7 6 f c . . 
+    . . . f c c c c 7 7 6 f 7 7 c . 
+    . . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+    . c 7 7 2 7 7 c f c 6 7 7 6 c c 
+    c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+    f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+    f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+    . f 6 1 1 1 1 1 1 6 6 6 f . . . 
+    . . c c c c c c c c c f . . . . 
+    `]
+SpawnLocation = tiles.getTilesByType(sprites.castle.tileGrass1)
+PlayerLevel = 0
+let NPC1 = sprites.create(img`
+    . . . . f f f f . . . . 
+    . . f f e e e e f f . . 
+    . f f e e e e e e f f . 
+    f f f f 4 e e e f f f f 
+    f f f 4 4 4 e e f f f f 
+    f f f 4 4 4 4 e e f f f 
+    f 4 e 4 4 4 4 4 4 e 4 f 
+    f 4 4 f f 4 4 f f 4 4 f 
+    f e 4 d d d d d d 4 e f 
+    . f e d d b b d d e f . 
+    . f f e 4 4 4 4 e f f . 
+    e 4 f b 1 1 1 1 b f 4 e 
+    4 d f 1 1 1 1 1 1 f d 4 
+    4 4 f 6 6 6 6 6 6 f 4 4 
+    . . . f f f f f f . . . 
+    . . . f f . . f f . . . 
+    `, SpriteKind.QuestNPC)
+let PlayerSprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(PlayerSprite)
+scene.cameraFollowSprite(PlayerSprite)
+tiles.setCurrentTilemap(tilemap`level1`)
+let PlayerHP = statusbars.create(100, 8, StatusBarKind.Health)
+let PlayerMagic = statusbars.create(100, 4, StatusBarKind.Magic)
+PlayerHP.positionDirection(CollisionDirection.Top)
+PlayerHP.setOffsetPadding(-30, 0)
+PlayerMagic.positionDirection(CollisionDirection.Top)
+PlayerMagic.setOffsetPadding(-30, 8)
+game.onUpdateInterval(600000, function () {
+	
+})
